a2c1cff2114633192dafdfb9ced144816b1da26b
Luke A. Guest <laguest@archeia.com>
2019-07-28 14:14:04 +0100

    Add Spanish and French.

--- hello/hello.go
+++ hello/hello.go
@@ -2,16 +2,28 @@ package main
 
 import "fmt"
 
+const spanish = "Spanish"
+const french = "French"
+const spanishHelloPrefix = "Hola, "
+const frenchhHelloPrefix = "Bonjour, "
 const englishHellPrefix = "Hello, "
 
-func Hello(name string) string {
+func Hello(name string, language string) string {
 	if name == "" {
 		name = "world"
+	}
+
+	if language == spanish {
+		return spanishHelloPrefix + name
+	}
+
+	if language == french {
+		return frenchhHelloPrefix + name
 	}
 
 	return englishHellPrefix + name
 }
 
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("world", ""))
 }
