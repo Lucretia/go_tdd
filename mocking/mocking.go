f0709f43c7b9f6d3bc04b5c188446fbf5b7a71df
Luke A. Guest <laguest@archeia.com>
2019-07-29 19:54:55 +0100

    Finiah up with a ConfigurableSleeper and test.

--- mocking/mocking.go
+++ mocking/mocking.go
@@ -16,11 +16,15 @@ type Sleeper interface {
 	Sleep()
 }
 
-type DefaultSleeper struct{}
-
-func (d *DefaultSleeper) Sleep() {
-	time.Sleep(1 * time.Second)
+type ConfigurableSleeper struct {
+	duration time.Duration
+	sleep    func(time.Duration)
 }
+
+func (c *ConfigurableSleeper) Sleep() {
+	c.sleep(c.duration)
+}
+
 func Countdown(out io.Writer, sleeper Sleeper) {
 	for i := coundownStart; i > 0; i-- {
 		sleeper.Sleep()
@@ -40,7 +44,7 @@ func Countdown(out io.Writer, sleeper Sleeper) {
 }
 
 func main() {
-	sleeper := &DefaultSleeper{}
+	sleeper := &ConfigurableSleeper{1 * time.Second, time.Sleep}
 
 	Countdown(os.Stdout, sleeper)
 }
