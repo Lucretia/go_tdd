7bf78cfd1969fe1cee172eb4291781f42d662dc3
Luke A. Guest <laguest@archeia.com>
2019-07-29 19:17:21 +0100

    Add the 1s delay.

--- mocking/mocking.go
+++ mocking/mocking.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"time"
 )
 
 const (
@@ -13,9 +14,11 @@ const (
 
 func Countdown(out io.Writer) {
 	for i := coundownStart; i > 0; i-- {
+		time.Sleep(1 * time.Second)
 		fmt.Fprintln(out, i)
 	}
 
+	time.Sleep(1 * time.Second)
 	fmt.Fprintf(out, finalWord)
 }
 
